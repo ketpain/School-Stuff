7504f2b6259741d0388ca3819400447fc26a8961
ketpain <42127943+ketpain@users.noreply.github.com>
2023-01-02 20:39:04 -0500

    Deleted stuff and learned cd paths

---
+++ CS Code Stuff/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wasn't this working??");
+            Console.WriteLine("y");
+        }
+    }
+}
