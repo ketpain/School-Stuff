cc356fe7181219355384a8aed3cd0f39e31b08d1
ketpain <42127943+ketpain@users.noreply.github.com>
2023-01-04 03:07:21 -0500

    Moved to a new folder
    Update Program.cs

---
+++ CsConsoleApp/Gameloop.cs
@@ -0,0 +1,14 @@
+namespace MainProgram
+{
+    class GameLoop
+    {
+        public void MainLoop()
+        {
+            while (true)
+            {
+                Console.WriteLine("yo");
+                break;
+            }
+        }
+    }
+}
---
+++ CsConsoleApp/Program.cs
@@ -0,0 +1,11 @@
+namespace MainProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GameLoop gameLoop = new GameLoop();
+            gameLoop.MainLoop();
+        }
+    }
+}
