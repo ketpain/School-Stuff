e122d8d08315f513902015450e1cc86be2c0327c
ketpain <42127943+ketpain@users.noreply.github.com>
2023-01-10 00:24:30 -0500

    Experimenting with lists

--- CsConsoleApp/Gameloop.cs
+++ CsConsoleApp/Gameloop.cs
@@ -2,13 +2,34 @@ namespace MainProgram
 {
     class GameLoop
     {
+        List<Weapon> weapons = new List<Weapon>()
+        {
+                new Weapon("Sword", 10),
+                new Weapon("Bow", 7)
+        };
+
         public void MainLoop()
         {
-            while (true)
+            foreach (Weapon weapon in weapons)
             {
-                Console.WriteLine("yo");
-                break;
+                Console.WriteLine($"{weapon.name} - Damage: {weapon.damage}");
             }
+        }
+    }
+
+    struct Weapon
+    {
+        public string name;
+        public int damage;
+        public Weapon(string Name, int Damage)
+        {
+            name = Name;
+            damage = Damage;
+        }
+
+        public void Suh()
+        {
+            Console.WriteLine("HUEHUEHE");
         }
     }
 }
